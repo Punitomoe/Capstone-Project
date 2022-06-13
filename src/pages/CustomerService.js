179a27fbe9fa7f93fb5c90eb4d3d313716002ba3
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-06-13 21:00:57 +0700

    fix the font and customerService page + responsivve too

--- src/pages/CustomerService.js
+++ src/pages/CustomerService.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import CService from '../assets/CSMaturedi.png';
+import backgroundCS from '../assets/backgroundCS.png';
+import admin from '../assets/admin.png';
+import asker from '../assets/asker.png';
+import Avatar from '@mui/material/Avatar';
 import '../style/CustomerService.css';
+import { Link } from 'react-router-dom';
 
 function CustomerService() {
   return (
     <div>
         <Navbar/>
-        <div className='customerService' style={{ backgroundImage: `url(${CService})`}}></div>
+        <div className='customerService' style={{ backgroundImage: `url(${backgroundCS})`}}>
+          <h1 className='contactUs'>Contact Us</h1>
+          <div className='csBox'>
+            <center><Avatar alt="" src={admin} sx={{ width: 56, height: 56 }}/></center>
+            <h1>Talk to a member of our service team</h1>
+            <p>We will help you with any obstacle you faced</p>
+            <center><Link to="">
+            <button>Contact Customer Service</button>
+            </Link></center>
+          </div>
+
+          <div className='csBox'>
+            <center><Avatar alt="" src={asker} sx={{ width: 56, height: 56 }}/></center>
+            <h1>Frequently Asked Questions</h1>
+            <p>You are not the only one who asking this, take a look the answer for your questions</p>
+            <center><Link to="">
+            <button>Contact Customer Service</button>
+            </Link></center>
+          </div>
+        </div>
         <Footer/>
     </div>
   )
