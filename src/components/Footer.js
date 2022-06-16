f7fa7c9443bb682a60f2693c656fa12a1f32e208
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-06-16 16:05:42 +0700

    fixed view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import '../style/Footer.css';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import logoFooter from '../assets/logoFooter.png';
+import React from "react";
+import "../style/Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import logoFooter from "../assets/logoFooter.png";
 
 function Footer() {
   return (
-    <div className='footer'>
-
-      <div className='footer_left'>
-        <img src={logoFooter}/>
+    <div className="footer">
+      <div className="footer_left">
+        <img src={logoFooter} />
         <p>Media Social Kami</p>
       </div>
 
-      <div className='footer_right'>
-            <InstagramIcon/>
-            <FacebookIcon/>
-            <LinkedInIcon/>
+      <div className="footer_right">
+        <InstagramIcon />
+        <FacebookIcon />
+        <LinkedInIcon />
         <p> &copy; 2022 maturedi.com </p>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/CustomerService.js
+++ src/pages/CustomerService.js
@@ -1,39 +1,55 @@
-import React from 'react';
-import Layout from '../layout/Layout';
-import backgroundCS from '../assets/backgroundCS.png';
-import admin from '../assets/admin.png';
-import asker from '../assets/asker.png';
-import Avatar from '@mui/material/Avatar';
-import '../style/CustomerService.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Layout from "../layout/Layout";
+import backgroundCS from "../assets/backgroundCS.png";
+import admin from "../assets/admin.png";
+import asker from "../assets/asker.png";
+import Avatar from "@mui/material/Avatar";
+import "../style/CustomerService.css";
+import { Link } from "react-router-dom";
 
 function CustomerService() {
   return (
     <div>
-        <Layout>
-        <div className='customerService' style={{ backgroundImage: `url(${backgroundCS})`}}>
-          <h1 className='contactUs'>Contact Us</h1>
-          <div className='csBox'>
-            <center><Avatar alt="" src={admin} sx={{ width: 56, height: 56 }}/></center>
+      <Layout>
+        <div
+          className="customerService"
+          style={{ backgroundImage: `url(${backgroundCS})` }}
+        >
+          <h1 className="contactUs">Contact Us</h1>
+          <div className="csBox">
+            <center>
+              <Avatar alt="" src={admin} sx={{ width: 56, height: 56 }} />
+            </center>
             <h1>Talk to a member of our service team</h1>
             <p>We will help you with any obstacle you faced</p>
             <center>
-            <button><a href='https://wa.me/qr/T6OD62J5F7MYF1'>Contact Customer Service</a></button>
+              <button>
+                <a href="https://wa.me/qr/T6OD62J5F7MYF1">
+                  Contact Customer Service
+                </a>
+              </button>
             </center>
           </div>
 
-          <div className='csBox'>
-            <center><Avatar alt="" src={asker} sx={{ width: 56, height: 56 }}/></center>
+          <div className="csBox">
+            <center>
+              <Avatar alt="" src={asker} sx={{ width: 56, height: 56 }} />
+            </center>
             <h1>Frequently Asked Questions</h1>
-            <p>You are not the only one who asking this, take a look the answer for your questions</p>
-            <center><Link to="/error">
-            <button>Go to Help Center</button>
-            </Link></center>
+            <p>
+              You are not the only one who asking this, take a look the answer
+              for your questions
+            </p>
+            <center>
+              <Link to="/error">
+                <button>Go to Help Center</button>
+              </Link>
+            </center>
           </div>
         </div>
-        </Layout>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default CustomerService
+export default CustomerService;
