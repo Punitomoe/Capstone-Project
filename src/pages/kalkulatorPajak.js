8228ec08c8aa9f486f83868d09477d61db90bc11
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-06-16 22:19:50 +0700

    add style in kalkulator page

--- src/pages/kalkulatorPajak.js
+++ src/pages/kalkulatorPajak.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import Layout from '../layout/Layout';
-import '../style/kalkulatorPajak.css';
+import React from "react";
+import Layout from "../layout/Layout";
+import "../style/kalkulatorPajak.css";
+import PajakKendaraan from "../assets/PajakKendaraan.png";
 
 function kalkulatorPajak() {
   return (
     <div>
       <Layout>
-        <div className='indexPajak'> 
-
+        <div className="indexPajak">
+          <div className="textPajak">
+            <h1> Kalkulator Pajak </h1>
+            <p>
+              {" "}
+              Hitung kisaran pajak yang harus kamu bayarkan di Kalkulator Pajak!{" "}
+            </p>
+          </div>
+          <div className="container">
+            <div className="box"></div>
+            <div className="box"></div>
+            <div className="box" style={{ backgroundImage: `url(${PajakKendaraan})` }}></div>
+          </div>
         </div>
       </Layout>
     </div>
-  )
+  );
 }
 
-export default kalkulatorPajak
+export default kalkulatorPajak;
