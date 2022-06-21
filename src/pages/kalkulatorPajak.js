f40fd819e6e1fd03565368c3b1f6fc167c4c7cfe
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-06-21 21:01:09 +0700

    fix the calculator

--- src/pages/kalkulatorPajak.js
+++ src/pages/kalkulatorPajak.js
@@ -1,10 +1,73 @@
-import React from "react";
 import Layout from "../layout/Layout";
 import "../style/kalkulatorPajak.css";
 import PajakKendaraan from "../assets/PajakKendaraan.png";
 import { useState } from "react";
 
-function kalkulatorPajak() {
+function KalkulatorPajak() {
+  const [penghasilan, setPenghasilan] = useState(0);
+  const [bonus, setBonus] = useState(0);
+  const [tanggungan, setTanggungan] = useState(0);
+  const [hasil, setHasil] = useState(0);
+  const [omzetValue, setOmzetValue] = useState(0);
+  const [omzetResult, setOmzetResult] = useState(0)
+
+
+  const handleTanggungan = (e) => {
+    const jenis = e.target.value;
+    switch (jenis) {
+      case "TK/0":
+        setTanggungan(0)
+        break;
+      case "K/0":
+        setTanggungan(4500000)
+        break;
+      case "K/1":
+        setTanggungan(9000000)
+        break;
+      case "K/2":
+        setTanggungan(13500000)
+        break;
+      case "K/3":
+        setTanggungan(18000000)
+        break;
+      default:
+        setTanggungan(0)
+        break;
+    }
+  }
+
+  const hitungHasil = () => {
+    let gajiBersih = (parseInt(penghasilan) + parseInt(bonus));
+    let tanggunganAkhir = parseInt(tanggungan) + 54000000;
+    let total = gajiBersih - tanggunganAkhir;
+
+
+    if (gajiBersih < 54000000) {
+      setHasil(0)
+    }
+
+    console.log(total);
+
+    if (total <= 50000000) {
+       setHasil(total * 0.05);
+    }else if(total <= 300000000){
+      setHasil((total - 50000000) * 0.15 + 2500000);
+    }else if(total <= 750000000){
+      setHasil((total - 300000000) * 0.25 + 25000000 + 37500000);
+    }else{
+      setHasil((total - 750000000) * 0.3 + 25000000 + 37500000 + 125000000);
+    }
+
+  }
+
+  const handleOmzet = () => {
+    if (omzetValue < 4800000000) {
+      setOmzetResult(omzetValue * 0.5);
+    }
+  }
+
+
+
   return (
     <div>
       <Layout>
@@ -22,28 +85,41 @@ function kalkulatorPajak() {
                 <h1>Pajak Karyawan</h1>
                 <div>
                   <label>Penghasilan dalam Setahun</label>
-                  <input id="penghasilan"/>
+                  <input id="penghasilan" type="number" onChange={(e) => setPenghasilan(e.target.value)} />
                   <br />
-                  <label>Bonus THR dsb</label><br/>
-                  <input id="thr"/>
-                  <br/>
-                  <label>Status Perkawinan/Tanggungan</label><br/>
-                  <select>
-                  <option value="tk0">TK/0</option>
-                  <option value="k0">K/0</option>
-                  <option value="k1">K/1</option>
-                  <option value="k2">K/2</option>
-                  <option value="k3">K/3</option>
-                  </select><br/>
+                  <label>Bonus THR dsb</label><br />
+                  <input id="thr" type="number" onChange={(e) => setBonus(e.target.value)} />
+                  <br />
+                  <label>Status Perkawinan/Tanggungan</label><br />
+                  <select onChange={handleTanggungan}>
+                    <option>TK/0</option>
+                    <option>K/0</option>
+                    <option>K/1</option>
+                    <option>K/2</option>
+                    <option>K/3</option>
+                  </select><br />
+                </div>
+                <div className="btn">
+                  <button onClick={hitungHasil}>Hitung</button>
                 </div>
                 <div>
                   <p>Jumlah Kisaran Pajak yang Harus dibayar:</p>
                   <br />
-                  <div className="result"></div>
+                  <div className="result">{hasil}</div>
                 </div>
               </div>
             </div>
-            <div className="box"></div>
+            <div className="box">
+              <h1>Pajak UMKM</h1>
+              <label>Omzet per tahun</label>
+              <input type="number" onChange={(e) => setOmzetValue(e.target.value)} />
+              <button onClick={handleOmzet}>Total</button>
+              <div>
+                  <p>Jumlah Kisaran Pajak yang Harus dibayar:</p>
+                  <br />
+                  <div className="result">{omzetResult}</div>
+                </div>
+            </div>
             <div
               className="box"
               style={{ backgroundImage: `url(${PajakKendaraan})` }}
@@ -55,4 +131,4 @@ function kalkulatorPajak() {
   );
 }
 
-export default kalkulatorPajak;
+export default KalkulatorPajak;
