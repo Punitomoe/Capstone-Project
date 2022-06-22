4b9ff72c674d741be24294d0624082267ec9c287
Dimas Julio Cessar <dimasjc08@gmail.com>
2022-06-22 13:37:15 +0700

    add pajak kendaraan page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Error from "./pages/Error";
 import CustomerService from "./pages/CustomerService";
 import KalkulatorPajak from "./pages/kalkulatorPajak";
+import PajakKendaraan from "./pages/PajakKendaraan";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/customerService" element={<CustomerService />} />
           <Route path="/kalkulatorPajak" element={<KalkulatorPajak />} />
           <Route path="/error" element={<Error />} />
+          <Route path="/menu/pajak/pajakKendaraan" element={<PajakKendaraan />} />
         </Routes>
       </BrowserRouter>
     </div>
