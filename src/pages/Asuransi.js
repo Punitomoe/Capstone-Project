f4f69be9e46073ee3cee70608ac9fd61a7552229
Rahmi Azizah <79822759+rahmi-na@users.noreply.github.com>
2022-06-30 02:36:49 +0700

    add page perpajakan, asuransi, and legal dokumen

--- src/pages/Asuransi.js
+++ src/pages/Asuransi.js
@@ -1,34 +1,31 @@
 import Layout from "../layout/Layout";
 import "../style/SubMenu.css";
-import Kendaraan from "../assets/perpajakan/Kendaraan.png";
-import Karyawan from "../assets/perpajakan/Karyawan.png";
-import Usaha from "../assets/perpajakan/Usaha.png";
+import Mobil from "../assets/asuransi/Mobil.png";
+import BPJS from "../assets/asuransi/BPJS.png";
 
-function Perpajakan() {
+function Asuransi() {
   return (
     <>
       <Layout>
         <div className="padding-y-xxl position-relative pt-3 pb-5 mb-5 z-index-1">
           <div className="max-width-adaptive-sm position-relative z-index-2 container">
             <div className="text-component margin-bottom-sm  pt-5 mb-5 text-center">
-              <h2 className="sub_menu_title">Perpajakan</h2>
+              <h2 className="sub_menu_title">Asuransi</h2>
             </div>
           </div>
           <div className="container">
-            <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
               <div className="col h-auto ">
                 <div className="card m-3 card-sub-menu border-0">
                   <div className="sub-menu">
                     <img
                       className="card-img-top img-sub-menu img-fluid"
-                      src={Kendaraan}
+                      src={Mobil}
                       alt=""
                     />
                   </div>
                   <div className="p-2">
-                    <h5 className="card-sub-title text-center">
-                      Pajak Kendaraan
-                    </h5>
+                    <h5 className="card-sub-title text-center">Asuransi Mobil</h5>
                   </div>
                 </div>
               </div>
@@ -37,28 +34,12 @@ function Perpajakan() {
                   <div className="sub-menu">
                     <img
                       className="card-img-top img-sub-menu img-fluid"
-                      src={Karyawan}
+                      src={BPJS}
                       alt=""
                     />
                   </div>
                   <div className="p-2">
-                    <h5 className="card-sub-title text-center">
-                      Pajak Karyawan
-                    </h5>
-                  </div>
-                </div>
-              </div>
-              <div className="col h-auto ">
-                <div className="card m-3 card-sub-menu border-0">
-                  <div className="sub-menu">
-                    <img
-                      className="card-img-top img-sub-menu img-fluid"
-                      src={Usaha}
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-2">
-                    <h5 className="card-sub-title text-center">Pajak Usaha</h5>
+                    <h5 className="card-sub-title text-center">BPJS Mobil</h5>
                   </div>
                 </div>
               </div>
@@ -70,4 +51,4 @@ function Perpajakan() {
   );
 }
 
-export default Perpajakan;
+export default Asuransi;
